2c60d7a18c3222c8de64a79b0464aabdb8d24444
sweet202020 <dolcealessio1@gmail.com>
2023-01-16 16:05:07 +0100

    add migrate to foreign key

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['sito vetrina', 'e-commerce', 'web app', 'mobile app', 'game', 'social network'];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name);
+            $newType->save();
+        }
+    }
+}
